285abeba206efafd6885d9a79693a09cfad83e3c
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-29 23:38:39 -0300

    Add User and StarList

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ActivityIndicator, Keyboard } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -19,6 +20,14 @@ import {
 } from './styles';
 
 export default class Main extends Component {
+  static navigationOptions = { title: 'GitHub RN' };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     users: [],
     newUser: '',
@@ -57,6 +66,12 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
   render() {
     const { users, newUser, loading } = this.state;
 
@@ -90,7 +105,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -100,5 +115,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = { title: 'GitHub RN' };
